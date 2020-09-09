a1eeb3e05b7738f8968994fa1dd367d0e6b1841e
Soumyadip Mondal <soumyadip.mondal@gmail.com>
2020-09-09 11:10:00 +0530

    Story 1109 :: Component Creation

---
+++ angular/src/app/receipes/receipe-list/receipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipe-list',
+  templateUrl: './receipe-list.component.html',
+  styleUrls: ['./receipe-list.component.scss']
+})
+export class ReceipeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
