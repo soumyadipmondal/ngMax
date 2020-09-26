b492efb1c28f9afad875c62b7e0e45a07fbc033a
Soumyadip Mondal <soumyadip.mondal@gmail.com>
2020-09-26 10:58:34 +0530

    Reactive FormsModule added

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +29,8 @@ import { ReceipeEditComponent } from './receipes/receipe-edit/receipe-edit.compo
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
